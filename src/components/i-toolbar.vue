e2e42ecace6718832775fc78bc001bbaa4acc62b
uioz <hzxbpld@163.com>
2019-04-14 23:20:56 +0800

    update i-toolbar

--- src/components/i-toolbar.vue
+++ src/components/i-toolbar.vue
@@ -1,12 +1,17 @@
 <docs>
 
   intro:
+  该组件用于内容区域的工具栏,在这个组件中依赖状态,来更新内部组件的状态.  
+  且会触发多种事件.  
 
   props:
+  edited:boolean=false // 内容是否被编辑(被编辑则保存激活)
 
   methods:
+  save():void // 当点击保存的时候触发
 
   slots:
+  default // 用于放置插槽右侧的内容
 
   process:
 
@@ -14,7 +19,10 @@
 <template>
   <mu-appbar color="grey800">
     <template #right>
-      <mu-button color="cyan500">保存</mu-button>
+      <mu-container>
+        <template></template>
+        <mu-button @click="$emit('onSave')" :disabled="!edited" color="cyan500">保存</mu-button>
+      </mu-container>
     </template>
   </mu-appbar>
 </template>
@@ -27,11 +35,9 @@ export default {
       default: false
     }
   },
-  methods: {}
 };
 </script>
 <style>
-.i-toolbar {
-}
+
 </style>
 
